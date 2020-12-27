4ea6108700e7ba3deafa50a49b98deda9f13eb92
Ausar Yao <ausaryao00@gmail.com>
2020-12-26 21:10:18 -0800

    Update: changed set up file to make it work

--- set_up.sh
+++ set_up.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-sudo ip link set wlan1 down	#set network interface down to switch operation mode
-sudo iw wlan1 set type ibss	#switch operation mode to ibss (ad-hoc mode)
-sudo ip link set wlan1 up	#set network inerface up to activate it
-sudo iw wlan1 ibss join mesh_test 2417 	#join/create a mesh netowrk with certain frequency (choose one with minimal interference)
-					#check supported frequencies with iw list and see for the interface you are using.
-					#ALL devices in the mesh network MUST have the same ssid and frequency
-
-#Next assign a static ip address to the interface for mesh network (in my case wlan1)
-# ip address add address/prefix_len broadcast + dev interface (https://wiki.archlinux.org/index.php/Network_configuration#Static_IP_address)
-#remember to use different ip address for different device
+sudo ifconfig wlan1 down					#set wlan1 (or your designated mesh network interface) down to switch operation mode
+sudo iwconfig wlan1 channel 1 essid mesh_test mode ad-hoc freq 2.417GHz	#switch operation mode to ad-hoc and create a mesh network
+sudo  ifconfig wlan1 up						#set network inerface up to activate it
+#sudo ifconfig wlan1 10.10.0.1 netmask 255.255.255.0	 	#assign unique ip address for each device
