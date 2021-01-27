4d8d8e10c16881761c0cb13101e295dddb178a07
ausar <ausar@nano.localdomain>
2021-01-26 16:38:12 -0800

    Updated set up script to adapt to debian kernel upgrade

--- set_up.sh
+++ set_up.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+	echo "Illegal number of parameters: specifiy network interface"
+	exit
+fi
 #Remember to check your wifi interface id, I would suggest using wlan0 
 #for Internet access, get an additional wifi dongle on wlan1
-sudo ifconfig wlan1 down					
+sudo ifconfig $1 down					
 #set wlan1 (or your designated mesh network interface) down to switch operation mode
-sudo iwconfig wlan1 channel 1 essid mesh_test mode ad-hoc freq 2.417GHz	
+sudo iwconfig $1 channel 1 mode ad-hoc freq 2.417GHz essid mesh_test
 #switch operation mode to ad-hoc and create a mesh network
-sudo  ifconfig wlan1 up						
+sudo  ifconfig $1 up						
 #set network inerface up to activate it
 read -r id < "node_id"
-sudo ifconfig wlan1 10.10.0.$id netmask 255.255.255.0	 	
+sudo ifconfig $1 10.10.0.$id netmask 255.255.255.0	 	
 #assign unique ip address for each device in /build/node_id file (first line)
